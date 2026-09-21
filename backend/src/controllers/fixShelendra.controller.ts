7740d70ae61ff53b1c604bf9318a3f1d6ee7b4db
divyayadav12 <divyayadav12@users.noreply.github.com>
2026-09-21 12:25:41 +0530

    fix: update query to use email for shailendra

--- backend/src/controllers/fixShelendra.controller.ts
+++ backend/src/controllers/fixShelendra.controller.ts
@@ -5,7 +5,7 @@ import User from '../models/User';
 export const fixShelendra = async (req: Request, res: Response) => {
   try {
     const user = await User.findOneAndUpdate(
-      { name: { $regex: /elendra/i } },
+      { email: 'fastchouhan2022@gmail.com' },
       { $set: { officeStartTime: '09:05', officeEndTime: '18:05' } },
       { new: true }
     );
